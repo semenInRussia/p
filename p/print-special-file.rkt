db2481f523331f610ebebe38bf971dd45012f05b
semenInRussia <hrams205@gmail.com>
2022-11-16 18:47:28 +0300

    [feature] able to print mama's pavlovsk documents

--- p/print-special-file.rkt
+++ p/print-special-file.rkt
@@ -8,6 +8,8 @@
 
 (define cs-document-filename (build-path mama-directory "Obelisk.docx.pdf"))
 (define cs-map-filename (build-path mama-directory "pushkin-maps.pdf"))
+(define pavlovsk-document-filename (build-path mama-directory
+                                               "found-garbage-in-pavlovsk.pdf"))
 
 (define/contract (print-cs amount-of-docs amount-of-maps)
   (-> (and/c number? positive?) (and/c number? positive?) boolean?)
@@ -15,6 +17,11 @@
   (2side-print-file cs-document-filename amount-of-docs)
   (printf "Print maps, ~a times\n" amount-of-maps)
   (print-file cs-map-filename #:amount amount-of-maps))
+
+(define/contract (print-pavlovsk amount-of-docs amount-of-maps)
+  (-> (and/c number? positive?) (and/c number? positive?) boolean?)
+  (printf "Print main document, ~a times\n" amount-of-maps)
+  (2side-print-file pavlovsk-document-filename amount-of-docs))
 
 (module+ main
   (require racket/cmdline)
@@ -37,6 +44,18 @@
                         "Amount of the maps to print."
                         (maps MAPS)])
       (print-cs (docs) (maps))]
+     [("pavlovsk" "p")
+      (define docs (make-parameter 1))
+      (define maps (make-parameter 1))
+
+      (command-line
+       #:program "print-cs"
+       #:argv argv
+       #:once-each
+       [("-d" "--documents" "--docs") DOCS
+                                      "Amount of the documents to print."
+                                      (docs DOCS)])
+      (print-pavlovsk (docs) (maps))]
      [else
       (displayln "This command excepts one argument: command")
       (displayln "Command can be one of the following things:")
